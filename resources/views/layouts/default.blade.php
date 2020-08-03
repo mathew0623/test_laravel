32138b612b13f091bb579480971635e7ebd8c1d4
mathew <mathew06233@gmail.com>
2020-08-03 17:12:04 +0800

    test test

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,23 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'TEST') - Laravel</title>
+    <title>@yield('title', 'TEST') - Laravel </title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">TEST</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">HELP</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">SIGN IN</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      @yield('content')
+    </div>
   </body>
 </html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+
+@section('content')
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      SEE <a href="https://learnku.com/courses/laravel-essential-training">Laravel </a> HOME PAGE
+    </p>
+    <p>
+      ZZZZZZZZZZZ
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">SIGN UP</a>
+    </p>
+  </div>
+@stop
