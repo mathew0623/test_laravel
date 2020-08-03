c0a468279ffedd78bfce0468872ee530eff1a67f
mathew <mathew06233@gmail.com>
2020-08-03 15:33:34 +0800

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Route;
+
+class StaticPagesController extends Controller
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+{
+    public function home()
+    {
+        return 'HOME';
+    }
+
+    public function help()
+    {
+        return 'HELP';
+    }
+
+
+    public function about()
+    {
+        return 'ABOUT';
+    }
+
+
+
+}
