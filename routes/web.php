15fbcb169d50ad75fef8e89d58fe8d6748dee86a
mathew <mathew06233@gmail.com>
2020-08-03 16:07:54 +0800

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
 Route::get('/', 'StaticPagesController@home');
+Route::get('/home', 'StaticPagesController@help');
 Route::get('/help', 'StaticPagesController@help');
 Route::get('/about', 'StaticPagesController@about');
