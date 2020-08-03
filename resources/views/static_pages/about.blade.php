887ac2d6cf2393fecd9299081b12b1db7bff03b0
mathew <mathew06233@gmail.com>
2020-08-03 16:02:49 +0800

    Initial commit

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title','ABOUT')
+
+@section('content')
+  <h1>ABOUT</h1>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title','HELP')
+
+@section('content')
+  <h1>HELP</h1>
+@stop
