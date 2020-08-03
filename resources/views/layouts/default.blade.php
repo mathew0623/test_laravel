7cecacfff823e7e441e35179bb0d9fb1c7a7a8e3
mathew <mathew06233@gmail.com>
2020-08-03 18:15:53 +0800

    test test

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,18 +2,18 @@
 <html>
   <head>
     <title>@yield('title', 'TEST') - Laravel </title>
-    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+    <link rel="stylesheet" href="{{mix('css/app.css') }}">
   </head>
 
-  <body>
+    <body>
       @include('layouts._header')
 
-    <div class="container">
-        <div class="offset-md-1 col-md-10">
-      @yield('content')
-      @include('layouts._footer')
-        </div>
-    </div>
+        <div class="container">
+         <div class="offset-md-1 col-md-10">
+             @yield('content')
+             @include('layouts._footer')
+         </div>
+      </div>
 
   </body>
 </html>
