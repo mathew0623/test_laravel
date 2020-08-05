5132abdc0eeaf5f94b8343984aafca0eedd6b0bd
mathew <mathew06233@gmail.com>
2020-08-05 11:55:20 +0800

    login

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+
+use Illuminate\Http\Request;
+
+
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            session()->flash('success', 'Welcome back!');
+            return redirect()->route('users.show',[Auth::user()]);
+        } else {
+            session()->flash('danger', 'Sorry, email or password incorrect');
+            return redirect()->back()->withInput();
+
+        }
+ 
+    }
+
+}
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -3,7 +3,7 @@
     <a class="navbar-brand" href="/">Laravel testing</a>
     <ul class="navbar-nav justify-content-end">
       <li class="nav-item"><a class="nav-link" href="{{route('help')}}">HELP</a></li>
-      <li class="nav-item" ><a class="nav-link" href="#">SIGN IN</a></li>
+      <li class="nav-item" ><a class="nav-link" href="{{route('login')}}">LOGIN</a></li>
     </ul>
   </div>
 </nav>
